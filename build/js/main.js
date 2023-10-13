188079b8da7f8a190f3b82387642f7af77d828cf
Badrinath1339 <badrinath.p@customfurnish.com>
2023-10-13 18:28:59 +0530

    7 completed

--- build/js/main.js
+++ build/js/main.js
@@ -1,15 +1,5 @@
 "use strict";
-//Index signatures
-// interface TransactionObj {
-//     Pizza: number,
-//     Books: number,
-//     Job: number
-// }
-/*
-interface TransactionObj {
-   readonly [index:string]: number
-}
-*/
+//Index signatures    obj[key]
 const todaysTransactions = {
     Pizza: -10,
     Books: -5,
@@ -18,3 +8,35 @@ const todaysTransactions = {
 console.log(todaysTransactions.Pizza);
 let prop = 'Pizz';
 console.log(todaysTransactions[prop]);
+const student = {
+    name: "Doug",
+    GPA: 3.5,
+    classes: [100, 200]
+};
+//now we can access a property that doesn't exist in the object
+console.log(student.test);
+for (const key in student) {
+    console.log(` ${key}:${student[key]} `); //with out index signature we can't use varible as key
+}
+//if you don't wan't to use index signatures, the following code will be useful
+//comment the index signature and check the following loop
+for (const key in student) {
+    console.log(` ${key}:${student[key]} `);
+}
+//if we don't know the type of student 
+Object.keys(student).map(key => {
+    console.log(student[key]);
+});
+const logStudentKey = (student, key) => {
+    console.log(`Student ${key}: ${student[key]}`);
+};
+logStudentKey(student, 'GPA');
+logStudentKey(student, 'name');
+const monthlyIncomes = {
+    salary: 50,
+    bonus: 100,
+    sidehustle: 250
+};
+for (const revenue in monthlyIncomes) {
+    console.log(monthlyIncomes[revenue]);
+}
